d93f40fad77866ce1af3918a17058c66d736c060
margaridaatrindade <104433154+margaridaatrindade@users.noreply.github.com>
2024-03-09 14:05:39 +0000

    Delete InteraçãocomComputadores directory

--- InteraçãocomComputadores/scripts/popup.js
+++
@@ -1,9 +0,0 @@
-var closePopup = document.getElementById("popupclose");
-var overlay = document.getElementById("overlay");
-var popup = document.getElementById("popup");
-if(closePopup != null){
-// Fecha popo-up
-closePopup.onclick = function(){
-  overlay.style.display = 'none';
-  popup.style.display = 'none';
-};}
